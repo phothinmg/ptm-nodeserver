0aecef91a0dc265eef8e3bda95bba5d4519063db
phothinmg <phothinmg@gmail.com>
2023-12-11 02:28:16 +0630

    modified:   index.d.ts

--- index.js
+++ index.js
@@ -2,51 +2,30 @@ import * as fs from "node:fs";
 import * as http from "node:http";
 import * as path from "node:path";
 
-/**
- * Creates a development server for serving static files based on the provided routes.
- *
- * @param {Array} routes - An array of routes to serve static files from.
- */
-export default function devserver(routes){
-    if(!routes){console.log('Please check option : routes !!')}
-    const folders = routes;
-    const server = http.createServer((req, res) => {
-    let found = false;
-    for (const folder of folders) {
-        if (req.url.startsWith(folder.route)) {
-          const filePath = path.join(folder.path, req.url.replace(folder.route, ''));
-          if (fs.statSync(filePath).isDirectory()) {
-            const indexPath = path.join(filePath, 'index.html');
-            if (fs.existsSync(indexPath)) {
-              fs.createReadStream(indexPath).pipe(res);
-              found = true;
-              break;
-            }
-          }
-          try {
-            if (fs.existsSync(filePath)) {
-            
-              fs.createReadStream(filePath).pipe(res);
-              found = true;
-              break;
-            }
-          } catch (err) {
-            
-            if (err.code === 'ENOENT' && err.syscall === 'stat' && err.path === 'favicon.ico') {
-              continue;
-            }
-           
-            res.statusCode = 500;
-            res.end('Internal Server Error');
-            return;
-          }
+
+export default function devserver(routes) {
+  if (!routes) {
+    console.log('Please check option: routes !!');
+  }
+
+  const folders = routes;
+  const server = http.createServer((req, res) => {
+    const foundFolder = folders.find((folder) => req.url.startsWith(folder.route));
+
+    if (foundFolder) {
+      const filePath = path.join(foundFolder.path, req.url.replace(foundFolder.route, ''));
+      if (fs.existsSync(filePath) && fs.statSync(filePath).isDirectory()) {
+        const indexPath = path.join(filePath, 'index.html');
+        if (fs.existsSync(indexPath)) {
+          fs.createReadStream(indexPath).pipe(res);
+          return;
         }
       }
-      if (!found) {
-        res.statusCode = 404;
-        res.end('Not found');
-      }
-    });
-    
-  return server
+    }
+
+    res.statusCode = 404;
+    res.end('Not found');
+  });
+
+  return server;
 }
